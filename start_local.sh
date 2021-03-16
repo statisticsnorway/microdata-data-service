e628389e2e73974c085270baeba5a54b6427493d
vak <vak@macbookpro.local>
2021-03-16 15:34:38 +0100

    Config for local url

--- start_local.sh
+++ start_local.sh
@@ -1,4 +1,5 @@
 cd data_service
 export BUCKET_NAME="data-service-bucket-microdata-poc"
-export DATA_SERVICE_URL="https://data-service.staging-bip-app.ssb.no"
+# export DATA_SERVICE_URL="https://data-service.staging-bip-app.ssb.no"
+export DATA_SERVICE_URL="http://127.0.0.1:8000"
 uvicorn main:data_service_app --reload
