e9ae1cc2372a45cec4c66617888f35c2f68a05fd
vak <vak@macbookpro.local>
2021-04-21 16:37:46 +0200

    More env properties in startup script.

--- start_local.sh
+++ start_local.sh
@@ -7,4 +7,8 @@ export DATA_SERVICE_URL="http://127.0.0.1:8000"
 
 export DATASTORE_ROOT="no_ssb_test"
 
+export GOOGLE_APPLICATION_CREDENTIALS=/hers/goes/your/KEYFILE.json
+
+export READ_FROM_BUCKET = 'true'
+
 uvicorn main:data_service_app --reload
