66dfbb8118cb15da72b4e4bd27487600ce2be662
vak <vak@macbookpro.local>
2021-04-26 15:24:57 +0200

    First working filter.

--- data_service/core/filters.py
+++ data_service/core/filters.py
@@ -6,72 +6,20 @@ from pyarrow.dataset import Expression
 def filter_by_time_period(parquet_file, start: int, stop: int):
 
     stop_missing: Expression = ~ds.field("stop_epoch_days").is_valid()
-
     start_epoch_le_start: Expression = ds.field('start_epoch_days') <= start
-
     start_epoch_ge_start: Expression = ds.field('start_epoch_days') >= start
-
     start_epoch_le_stop: Expression = ds.field('start_epoch_days') <= stop
-
     start_epoch_g_start: Expression = ds.field('start_epoch_days') > start
-
     stop_epoch_ge_start: Expression = ds.field('stop_epoch_days') >= start
-
     stop_epoch_le_stop: Expression = ds.field('stop_epoch_days') <= stop
 
-    stop_epoch_e_stop: Expression = ds.field('stop_epoch_days') == 8065
-
-    find_by_time_period_filter = [  # ingen treff
-        [('start_epoch_days', '<=', start), stop_missing],
-        [('start_epoch_days', '<=', start), ('stop_epoch_days', ">=", start)],
-        [('start_epoch_days', '>=', start), ('start_epoch_days', '<=', stop)],
-        [('start_epoch_days', '>', start), ('stop_epoch_days', "<=", stop)]
-    ]
-
-    find_by_time_period_filter_2 = [  # ingen treff
-        [start_epoch_le_start, stop_missing],
-        [start_epoch_le_start, stop_epoch_ge_start],
-        [start_epoch_ge_start, start_epoch_le_stop],
-        [start_epoch_g_start, stop_epoch_le_stop]
-    ]
-
-    # ingen treff
-    find_by_time_period_filter_3 = \
-        [(start_epoch_le_start and stop_missing) or
-         (start_epoch_le_start and stop_epoch_ge_start) or
-         (start_epoch_ge_start and start_epoch_le_stop) or
-         (start_epoch_g_start and stop_epoch_le_stop)]
-
-    # riktig : 4 treff
-    #    find_by_time_period_filter_4 = [('start_epoch_days', '<=', start)]
-    # feil : ingen treff
-    #    find_by_time_period_filter_4 = ('start_epoch_days', '<=', start)
-
-    # riktig : 3 treff
-    find_by_time_period_filter_5 = stop_missing
-
-    # find_by_time_period_filter = (start_epoch_le_start and stop_missing) #or (start_epoch_le_start and stop_epoch_ge_start)
-
-    gir_feil_treff = \
-        ((start_epoch_le_start and stop_missing) or (start_epoch_le_start and stop_epoch_ge_start))
-
-    find_by_time_period_halv_filter_1 = (start_epoch_ge_start and stop_missing)
-
-    # SE HER!! Ulik resultat avhengig av [ ]
-    gir_ingen_treff = \
-        [(start_epoch_le_start and stop_epoch_ge_start)]
-
-    gir_feil_treff_2 = (start_epoch_le_start and stop_epoch_ge_start)
-    # 1000000003     2              7701             7956
-
-    gir_også_feil_treff = (start_epoch_ge_start and stop_epoch_le_stop)
-
-    print(start)
-    print(stop)
-
+    find_by_time_period_filter = (start_epoch_le_start & stop_missing) | \
+                                 (start_epoch_le_start & stop_epoch_ge_start) | \
+                                 (start_epoch_ge_start & start_epoch_le_stop) | \
+                                 (start_epoch_g_start & stop_epoch_le_stop)
     table = None
     try:
-        table = pq.read_table(source=parquet_file, filters=gir_feil_treff_2)
+        table = pq.read_table(source=parquet_file, filters=find_by_time_period_filter)
     except:
-        print('Ingen treff')
+        print('Empty resultset')
     return table
--- tests/unit/core/test_filters.py
+++ tests/unit/core/test_filters.py
@@ -1,44 +1,12 @@
-import unittest
 import glob
-import pandas
-import pyarrow.parquet as pq
+import unittest
+
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
 from data_service.core.filters import filter_by_time_period
 from tests.unit.util.util import convert_csv_to_parquet
-
-# FRA Datastore-API
-#
-# INSERT INTO `BOSTED__3_2`(part_num, unit_id, value, start, stop, start_type, stop_type, attributes)
-# VALUES
-# (0, 1000000001, '3', '1985-01-01', '1991-06-30', 20, NULL, NULL),
-# (80, 1000000001, '16', '1991-07-01', '1992-03-31', 20, NULL, NULL),
-# (130, 1000000001, '3', '1992-04-01', NULL, 20, NULL, NULL),
-# (180, 1000000002, '8', '1974-01-01', '1992-01-31', 20, NULL, NULL),
-# (230, 1000000002, '8', '1992-02-01', NULL, 20, NULL, NULL),
-# (299, 1000000003, '12', '1981-01-01', '1991-01-31', 20, NULL, NULL),
-# (450, 1000000003, '2', '1991-02-01', '1991-10-14', 20, NULL, NULL),
-# (499, 1000000003, '12', '1991-10-15', NULL, 20, NULL, NULL),
-# (999, 1000000004, '2', '1979-01-01', '1991-02-10', 40, NULL, NULL),
-# ;
-#
-#
-#
-#
-# private static final DATUM1 = new Datum(1000000001, '3', LocalDate.of(1985, 1, 1), LocalDate.of(1991, 6, 30))
-# private static final DATUM2 = new Datum(1000000002, '8', LocalDate.of(1974, 1, 1), LocalDate.of(1992, 1, 31))
-# private static final DATUM3 = new Datum(1000000003, '12', LocalDate.of(1981, 1, 1), LocalDate.of(1991, 1, 31))
-# private static final DATUM4 = new Datum(1000000004, '2', LocalDate.of(1979, 1, 1), LocalDate.of(1991, 2, 10))
-# private static final DATUM5 = new Datum(1000000003, '2', LocalDate.of(1991, 2, 1), LocalDate.of(1991, 10, 14))
-# private static final DATUM6 = new Datum(1000000003, '12', LocalDate.of(1991, 10, 15), null)
-# private static final DATUM7 = new Datum(1000000001, '16', LocalDate.of(1991, 7, 1), LocalDate.of(1992, 3, 31))
-# private static final DATUM8 = new Datum(1000000002, '8', LocalDate.of(1992, 2, 1), null)
-# private static final DATUM9 = new Datum(1000000001, '3', LocalDate.of(1992, 4, 1), null)
-#
-#
-#
-# EventQuery query = createEventQuery('1991-01-01', '1991-12-31', 'BOSTED')
-#
-# Collection<Datum> expectedDatums = [DATUM1, DATUM7, DATUM2, DATUM3, DATUM5, DATUM6, DATUM4]
-#
 
 
 class TestFilters(unittest.TestCase):
@@ -46,25 +14,21 @@ class TestFilters(unittest.TestCase):
     def test_filter_by_time_period(self):
         # ../../tests/resources/unit_test_data/TEST_BOSTED__3_2.csv
         # Dette på grunn av OSError: [Errno 30] Read-only file system: '/resources'
-        csv_file = 'EXPERIMENTAL__3_2.csv'
-        parquet_path = 'EXPERIMENTAL__3_2'
-
+        csv_file = 'TEST_BOSTED__3_2.csv'
+        parquet_path = 'TEST_BOSTED__3_2'
 
         convert_csv_to_parquet(csv_file, parquet_path, False)
-        parquet_file = glob.glob("EXPERIMENTAL__3_2/*.parquet")[0]
+        parquet_file = glob.glob("TEST_BOSTED__3_2/*.parquet")[0]
 
         # parquet_file = 'TEST_BOSTED__3_2/1a5324c3d6844e1788f6719b934dc069.parquet'
         print(parquet_file)
 
-        result_table = filter_by_time_period(parquet_file, 7670, 8034)
+        expected = pd.DataFrame(
+            {'unit_id': pd.Series([1000000001, 1000000001, 1000000002, 1000000003, 1000000003, 1000000003, 1000000004],
+                                  dtype='uint64'),
+             'value': pd.Series(["3", "16", "8", "12", "2", "12", "2"], dtype='str'),
+             'start_epoch_days': pd.Series([5479, 7851, 1461, 4018, 7701, 7957, 3287], dtype='int16'),
+             'stop_epoch_days': pd.Series([7850, 8125, 8065, 7700, 7956, np.nan, 7710], dtype='float')})
 
-        if (result_table is not None):
-            pq.write_table(result_table, "TEST_BOSTED__3_2/result.parquet")
-
-            parquet_table = pq.read_table(source="TEST_BOSTED__3_2/result.parquet")
-
-            print('Reultat dataset')
-            print('-----------------------------------------------------------')
-            print(parquet_table.to_pandas().head(10))
-            print('-----------------------------------------------------------')
-
+        result = filter_by_time_period(parquet_file, 7670, 8034)
+        assert_frame_equal(result.to_pandas(), expected)
