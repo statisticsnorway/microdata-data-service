70750f92ed17dcf287c61954bac2252c90e2c7a5
vak <vak@macbookpro.local>
2021-05-03 13:29:18 +0200

    Fixed api and processor for all endpoints.

--- data_service/api/data_api.py
+++ data_service/api/data_api.py
@@ -3,7 +3,7 @@ import logging
 from fastapi import APIRouter, Depends
 from fastapi.responses import FileResponse
 
-from data_service.api.query_models import InputTimePeriodQuery, QueryValidator
+from data_service.api.query_models import InputTimePeriodQuery, QueryValidator, InputTimeQuery, InputFixedQuery
 from data_service.config import config
 from data_service.config.config import get_settings
 from data_service.core import processor
@@ -27,15 +27,53 @@ def create_result_set_event_data(input_query: InputTimePeriodQuery, settings: co
     """
      Create result set of data with temporality type event.
 
-     - **input_query**: InputQuery as JSON
+     - **input_query**: InputTimePeriodQuery as JSON
      - **settings**: config.Settings object
      """
     log = logging.getLogger(__name__)
     log.info(f'Entering /data/event with input query: {input_query}')
     QueryValidator.validate(input_query)
 
-    result_filename = processor.process(input_query, settings)
-    log.info(f'Filename with the result set: {result_filename}')
+    result_filename = processor.process_event_request(input_query, settings)
+    log.info(f'Filename with event result set: {result_filename}')
+
+    return {'name': input_query.dataStructureName,
+            'dataUrl': settings.DATA_SERVICE_URL + '/retrieveResultSet?file_name=' + result_filename}
+
+
+@data_router.post("/data/status")
+def create_result_set_status_data(input_query: InputTimeQuery, settings: config.Settings = Depends(get_settings)):
+    """
+     Create result set of data with temporality type status.
+
+     - **input_query**: InputTimeQuery as JSON
+     - **settings**: config.Settings object
+     """
+    log = logging.getLogger(__name__)
+    log.info(f'Entering /data/status with input query: {input_query}')
+    QueryValidator.validate(input_query)
+
+    result_filename = processor.process_status_request(input_query, settings)
+    log.info(f'Filename with status result set: {result_filename}')
+
+    return {'name': input_query.dataStructureName,
+            'dataUrl': settings.DATA_SERVICE_URL + '/retrieveResultSet?file_name=' + result_filename}
+
+
+@data_router.post("/data/fixed")
+def create_result_set_fixed_data(input_query: InputFixedQuery, settings: config.Settings = Depends(get_settings)):
+    """
+     Create result set of data with temporality type fixed.
+
+     - **input_query**: InputFixedQuery as JSON
+     - **settings**: config.Settings object
+     """
+    log = logging.getLogger(__name__)
+    log.info(f'Entering /data/fixed with input query: {input_query}')
+    QueryValidator.validate(input_query)
+
+    result_filename = processor.process_fixed_request(input_query,settings)
+    log.info(f'Filename with fixed result set: {result_filename}')
 
     return {'name': input_query.dataStructureName,
             'dataUrl': settings.DATA_SERVICE_URL + '/retrieveResultSet?file_name=' + result_filename}
