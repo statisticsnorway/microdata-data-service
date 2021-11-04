9e6d26c164126ffdaf5186b88976241ad6109109
DanielElisenberg <daniel@elisenberg.com>
2021-11-04 23:45:40 +0100

    more_unit_testing: add test data

--- tests/resources/test_data.py
+++ tests/resources/test_data.py
@@ -1,4 +1,8 @@
 from datetime import datetime, timedelta
+from data_service.api.query_models import (
+    InputTimePeriodQuery, InputTimeQuery, InputFixedQuery
+)
+
 
 valid_jwt_payload = {
     "aud": "datastore",
@@ -22,3 +26,57 @@ jwt_payload_expired = {
     "exp": (datetime.now() - timedelta(hours=1)).timestamp(),
     "sub": "testuser"
 }
+
+PERSON_INCOME_ALL = (
+    ";unit_id;value\n"
+    "0;11111111864482;21529182\n"
+    "1;11111112296273;12687840\n"
+    "2;11111113785911;16354872\n"
+    "3;11111113735577;12982099\n"
+    "4;11111111454434;19330053\n"
+    "5;11111111190644;11331198\n"
+    "6;11111113331169;4166169\n"
+    "7;11111111923572;7394257\n"
+    "8;11111112261125;6926636\n"
+)
+
+PERSON_INCOME_LAST_ROW = (
+    ";unit_id;value\n"
+    "0;11111112261125;6926636\n"
+)
+
+VALID_EVENT_QUERY_PERSON_INCOME_ALL = InputTimePeriodQuery(
+    dataStructureName="TEST_PERSON_INCOME",
+    version="1.0.0.0",
+    startDate=1,
+    stopDate=32767
+)
+
+INVALID_EVENT_QUERY_INVALID_STOP_DATE = InputTimePeriodQuery(
+    dataStructureName="TEST_PERSON_INCOME",
+    version="1.0.0.0",
+    startDate=100,
+    stopDate=99
+)
+
+VALID_STATUS_QUERY_PERSON_INCOME_LAST_ROW = InputTimeQuery(
+    dataStructureName="TEST_PERSON_INCOME",
+    version="1.0.0.0",
+    date=12200
+)
+
+INVALID_STATUS_QUERY_NOT_FOUND = InputTimeQuery(
+    dataStructureName="NOT_A_DATASET",
+    version="1.0.0.0",
+    date=123
+)
+
+VALID_FIXED_QUERY_PERSON_INCOME_ALL = InputFixedQuery(
+    dataStructureName="TEST_PERSON_INCOME",
+    version="1.0.0.0"
+)
+
+INVALID_FIXED_QUERY_NOT_FOUND = InputFixedQuery(
+    dataStructureName="NOT_A_DATASET",
+    version="1.0.0.0"
+)
