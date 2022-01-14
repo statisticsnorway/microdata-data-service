8e4801404597af42a3081282259aaad33fc0b637
pawbu <pawbu@users.noreply.github.com>
2022-01-14 13:39:24 +0100

    Handle case when version is 0.0.0

--- data_service/adapters/storage/local.py
+++ data_service/adapters/storage/local.py
@@ -18,24 +18,30 @@ class LocalFileAdapter(FileAdapter):
 
     def get_file_path(self, data_structure_name: str, version: str) -> str:
         version_underscored = version.replace('.', '_')[:5]
-        data_versions = self.__get_data_versions__(version_underscored)
+        path_prefix = f"{self.settings.DATASTORE_DIR}/data/{data_structure_name}"
 
-        if data_structure_name not in data_versions:
-            raise NotFoundException(
-                f"No such data structure in data_versions file for version {version}")
+        if version == "0.0.0":
+            parquet_file_path = f"{data_structure_name}__{version_underscored[:3]}"
+            full_path = (
+                f"{path_prefix}/{parquet_file_path}"
+            )
+            full_path = full_path if os.path.isdir(full_path) else f"{full_path}.parquet"
+        else:
+            data_versions = self.__get_data_versions__(version_underscored)
+            if data_structure_name not in data_versions:
+                raise NotFoundException(
+                    f"No such data structure in data_versions file for version {version}")
+            parquet_file_path = data_versions[data_structure_name]
 
-        parquet_file_path = data_versions[data_structure_name]
+            full_path = (
+                f"{path_prefix}/{parquet_file_path}"
+            )
 
-        full_parquet_file_path = (
-            f"{self.settings.DATASTORE_DIR}/data/"
-            f"{data_structure_name}/{parquet_file_path}"
-        )
-
-        if not os.path.exists(full_parquet_file_path):
+        if not os.path.exists(full_path):
             self.log.error(f"Path {parquet_file_path} not does not exist")
             raise NotFoundException("No such data structure")
 
-        return full_parquet_file_path
+        return full_path
 
     def __get_data_versions__(self, version_underscored: str) -> str:
         data_versions_file = (
---
+++ tests/unit/storage/test_local.py
@@ -0,0 +1,13 @@
+from data_service.adapters.storage.local import LocalFileAdapter
+from data_service.config import config
+
+
+local_file_adapter = LocalFileAdapter(
+    settings=config.LocalFileSettings(
+        DATASTORE_DIR='tests/resources/datastore_unit_test',
+    ))
+
+
+def test_get_file_path():
+    assert local_file_adapter.get_file_path("TEST_PERSON_INCOME", "0.0.0") \
+            == "tests/resources/datastore_unit_test/data/TEST_PERSON_INCOME/TEST_PERSON_INCOME__0_0.parquet"
