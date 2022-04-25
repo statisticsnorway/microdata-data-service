76b5e7463baba0b5685acae9764c17c066336b51
DanielElisenberg <daniel@elisenberg.com>
2022-04-25 10:32:22 +0200

    4_part_semantic_version: update unit tests

--- tests/unit/storage/test_local.py
+++ tests/unit/storage/test_local.py
@@ -1,6 +1,13 @@
 from data_service.adapters.storage.local import LocalFileAdapter
 from data_service.config import config
 
+TEST_PERSON_INCOME_PATH = (
+    "tests/resources/datastore_unit_test/data/"
+    "TEST_PERSON_INCOME/TEST_PERSON_INCOME"
+)
+TEST_PERSON_INCOME_PATH_1_0 = f"{TEST_PERSON_INCOME_PATH}__1_0.parquet"
+TEST_PERSON_INCOME_PATH_DRAFT = f"{TEST_PERSON_INCOME_PATH}__DRAFT.parquet"
+
 local_file_adapter = LocalFileAdapter(
     settings=config.LocalFileSettings(
         DATASTORE_DIR='tests/resources/datastore_unit_test',
@@ -8,16 +15,25 @@ local_file_adapter = LocalFileAdapter(
 
 
 def test_get_file_path():
-    assert local_file_adapter.get_parquet_file_path("TEST_PERSON_INCOME", "0.0.0.1") \
-           == "tests/resources/datastore_unit_test/data/TEST_PERSON_INCOME/TEST_PERSON_INCOME__0_0.parquet"
+    assert TEST_PERSON_INCOME_PATH_1_0 == (
+        local_file_adapter.get_parquet_file_path(
+            "TEST_PERSON_INCOME", "1.0.0.0"
+        )
+     )
 
 
 def test_get_file_path_draft():
-    assert local_file_adapter.get_parquet_file_path("TEST_PERSON_INCOME", "draft") \
-           == "tests/resources/datastore_unit_test/data/TEST_PERSON_INCOME/TEST_PERSON_INCOME__0_0.parquet"
+    assert TEST_PERSON_INCOME_PATH_DRAFT == (
+        local_file_adapter.get_parquet_file_path(
+            "TEST_PERSON_INCOME", "0.0.0.1"
+        )
+     )
 
 
 def test_to_underscored_version():
-    assert "13_0_0" == local_file_adapter.__to_underscored_version__("13.0.0.0")
-    assert "1_0_0" == local_file_adapter.__to_underscored_version__("1.0.0.0")
-    assert "2_0_0" == local_file_adapter.__to_underscored_version__("2.0.0")
+    assert local_file_adapter._LocalFileAdapter__to_underscored_version(
+        "13.0.0.0"
+    ) == "13_0"
+    assert local_file_adapter._LocalFileAdapter__to_underscored_version(
+        "1.0.0.0"
+    ) == "1_0"
