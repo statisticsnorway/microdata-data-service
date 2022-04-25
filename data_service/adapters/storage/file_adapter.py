abdc9f3c258dcae9204bd82d7f86e6e3adfe9867
DanielElisenberg <daniel@elisenberg.com>
2022-04-25 10:31:37 +0200

    4_part_semantic_version: update storage adapters

--- data_service/adapters/storage/file_adapter.py
+++ data_service/adapters/storage/file_adapter.py
@@ -4,5 +4,17 @@ from abc import ABC, abstractmethod
 class FileAdapter(ABC):
 
     @abstractmethod
-    def get_parquet_file_path(self, data_structure_name: str, version: str) -> str:
+    def get_parquet_file_path(self, data_structure_name: str,
+                              version: str) -> str:
+        """
+        Returns the parquet file path for the given parameters.
+
+            Parameters:
+                data_structure_name (str): The name of the data structure
+
+                version (str): A 4 part semantic version ex.: 1.0.0.0
+
+            Returns:
+                parquet file path (str)
+        """
         pass
