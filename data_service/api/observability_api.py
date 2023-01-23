db1eacdc913be3829d8ad4791df41f10137a5a4e
DanielElisenberg <Daniel@Elisenberg.com>
2023-01-23 14:27:14 +0100

    refactor_app_structure: api cleanup

--- data_service/api/observability_api.py
+++ data_service/api/observability_api.py
@@ -1,3 +1,4 @@
+# pylint: disable=unused-argument
 from fastapi import APIRouter, Request
 
 
@@ -6,15 +7,13 @@ observability_router = APIRouter()
 
 @observability_router.get('/health/alive')
 def alive(
-        # needed for json_logging.get_correlation_id to work correctly
-        request: Request  # pylint: disable=unused-argument
+        request: Request  # needed for json_logging.get_correlation_id
 ):
     return "I'm alive!"
 
 
 @observability_router.get('/health/ready')
 def ready(
-        # needed for json_logging.get_correlation_id to work correctly
-        request: Request  # pylint: disable=unused-argument
+        request: Request  # needed for json_logging.get_correlation_id
 ):
     return "I'm ready!"
